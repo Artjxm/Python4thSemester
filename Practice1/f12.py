4bb05864d7e94040be8798a71e7ac6c3835d94c7
ShadonPC <55762492+Shad0n@users.noreply.github.com>
2021-02-21 18:23:01 +0300

    f3 done; f1, f2: pow() changed for **, print's wiped.

--- Practice1/f12.py
+++ Practice1/f12.py
@@ -3,21 +3,17 @@ import math
 
 def f12(x):
     if x < 46:
-        result = 57 * pow(x, 4) + x / 21
+        result = 57 * x ** 4 + x / 21
         return '%.2e' % result
     elif x < 139:
-        result = 73 * pow(x, 5) - math.cos(x)
+        result = 73 * x ** 5 - math.cos(x)
         return '%.2e' % result
     elif x < 184:
-        result = pow(x, 3) + x - math.sin(x)
+        result = x ** 3 + x - math.sin(x)
         return '%.2e' % result
     elif x < 246:
-        result = pow(x, 6) - pow(x, 7)
+        result = x ** 6 - x ** 7
         return '%.2e' % result
     else:
-        result = 87 * pow(x, 3) - math.sin(x) - 73
+        result = 87 * x ** 3 - math.sin(x) - 73
         return '%.2e' % result
-
-
-print(f12(141))
-print(f12(16))
