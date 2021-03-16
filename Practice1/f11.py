f623b8f5e8e30a8fefc5cd6b772c93ff3a74e9d7
ShadonPC <55762492+Shad0n@users.noreply.github.com>
2021-03-16 20:24:34 +0300

    f22 done (+f11 latest)

--- Practice1/f11.py
+++ Practice1/f11.py
@@ -2,8 +2,14 @@ import math
 
 
 def f11(x, y, z):
-    dec1 = (57 * y ** 4 + math.sin(z)) / \
-           (21 * y ** 5 - 30 * z * z)
-    sub = z ** 7 - 53 * y ** 8
-    dec2 = (x ** 4 - math.sin(z)) / y ** 6 - x ** 7
-    return dec1 - sub + dec2
+    part1 = (57 * y ** 4 + math.sin(z)) / (21 * y ** 5 - 30 * z ** 2)
+
+    part2 = z ** 7 - 53 * y ** 8
+
+    part3 = (x ** 4 - math.sin(z)) / (y ** 6 - x ** 7)
+
+    return part1 - part2 + part3
+
+
+# print(f11(15, 59, 59))
+# print(f11(-59, -54, 17))
